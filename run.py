2c6ec3638486a59dad9940bccdda2ede1e0d2d35
Ali jutt <85545453+alijutt07@users.noreply.github.com>
2022-04-16 17:36:02 +0500

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from test import main_bnsbuy
+    from Test import main_bnsbuy
     main_bnsbuy()
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
